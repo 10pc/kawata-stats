377c8368a0785b22b5beb8ece52bfd7e045aee02
10pc <10en.percent@gmail.com>
2024-05-14 08:42:14 +0700

    revert back

--- api.js
+++ api.js
@@ -3,10 +3,10 @@ import cheerio from 'cheerio';
 
 export const getUser = async (username, playmode = 'std') => {
 	const playmodes = {
-		std: '0',
-		taiko: '1',
-		catch: '2',
-		mania: '3',
+		std: 'osu',
+		taiko: 'taiko',
+		catch: 'fruits',
+		mania: 'mania',
 	}
 	if (!playmodes[playmode]){
 		return {
@@ -17,7 +17,7 @@ export const getUser = async (username, playmode = 'std') => {
 	try {
 		response = await got({
 			method: 'get',
-			url: `https://kawata.pw/users/${username}?mode=${playmodes[playmode]}`,
+			url: `https://osu.ppy.sh/users/${username}/${playmodes[playmode]}`,
 		});	
 	} catch (error) {
 		if (error.response.statusCode === 404){
@@ -32,7 +32,7 @@ export const getUser = async (username, playmode = 'std') => {
 	
     const body = response.body;
 	let $ = cheerio.load(body);
-	const data = JSON.parse($('.js-react--profile-page.ds').attr('data-initial-data'));
+	const data = JSON.parse($('.js-react--profile-page.osu-layout').attr('data-initial-data'));
 	data.current_mode = playmode;
 	return data;
 }
